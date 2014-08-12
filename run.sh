1a5615feec0f26c735673e5b7d5249de5638707d
julien <julien@twitter.com>
2014-08-12 15:06:55 -0700

    fix scripts

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 echo "compile"
 mvn clean compile || exit 1
 echo "run ntuple.TryNTuple"
-scala -classpath jaqen/target/classes:jaqen-test/target/classes com.twitter.jaqen.ntuple.TryNTuple
+scala -classpath jaqen-ntuple/target/classes:jaqen-test/target/classes com.twitter.jaqen.ntuple.TryNTuple
 echo "run ntuple.DemoNTuple"
-scala -classpath jaqen/target/classes:jaqen-test/target/classes com.twitter.jaqen.ntuple.DemoNTuple
+scala -classpath jaqen-ntuple/target/classes:jaqen-test/target/classes com.twitter.jaqen.ntuple.DemoNTuple
